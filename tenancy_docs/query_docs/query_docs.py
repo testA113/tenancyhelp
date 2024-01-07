8c9b1360f53565a6ec5facbcf2805a50c0446782
testA113 <83188384+testA113@users.noreply.github.com>
2024-01-07 19:36:10 +1300

    feat(indexing): separate indexes by source

--- tenancy_docs/query_docs/query_docs.py
+++ tenancy_docs/query_docs/query_docs.py
@@ -1,26 +1,20 @@
-import logging
-import os
 import dotenv
 from llama_index import (
-    OpenAIEmbedding,
     VectorStoreIndex,
     ServiceContext,
 )
 from llama_index.vector_stores import ChromaVectorStore
-from llama_index.storage.storage_context import StorageContext
-from llama_index.embeddings import HuggingFaceEmbedding
 import chromadb
 
 from tenancy_docs.index_docs.utils import get_embed_model
 
 
 def query_docs():
-    os.environ["TOKENIZERS_PARALLELISM"] = "false"
     embed_model = get_embed_model()
 
     # load from disk
     chroma_client = chromadb.PersistentClient(path="../index_docs/chroma_db")
-    chroma_collection = chroma_client.get_or_create_collection("tenancy_docs")
+    chroma_collection = chroma_client.get_or_create_collection("tenancy_services_docs")
     vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
     service_context = ServiceContext.from_defaults(embed_model=embed_model)
     index = VectorStoreIndex.from_vector_store(
@@ -29,15 +23,21 @@ def query_docs():
     )
 
     # Query Data
-    query_engine = index.as_query_engine()
+    query_engine = index.as_query_engine(similarity_top_k=3, streaming=True)
     response = query_engine.query(
-        "I accidentally sent the house on fire. Am I responsible for costs?"
+        "I accidentally set the house on fire. Am I responsible for costs?"
     )
     nodes = response.source_nodes
+    if len(nodes) == 0:
+        print("No results found")
+        return
     doc_metadata = [node.metadata for node in nodes]
+    doc_similarity_scores = [node.score for node in nodes]
     print(doc_metadata)
     print("\n")
-    print(response.response)
+    # stream the results
+    print("Streaming response...")
+    response.print_response_stream()
 
 
 if __name__ == "__main__":
