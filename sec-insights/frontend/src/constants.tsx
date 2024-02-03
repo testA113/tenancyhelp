c5a6488d680ff5a48e6fff2dc328c0745b742447
testA113 <83188384+testA113@users.noreply.github.com>
2024-02-03 13:20:56 +1300

    feat(api): sec insights starter

---
+++ sec-insights/frontend/src/constants.tsx
@@ -0,0 +1,5 @@
+export const GOOGLE_ANALYTICS_ID = "G-LGHB46ZGWR";
+export const INTERCOM_ID = "rx71g1uo";
+// TODO: Populate with your own Sentry DSN:
+// https://docs.sentry.io/product/sentry-basics/concepts/dsn-explainer/
+export const SENTRY_DSN: string | undefined = undefined;
