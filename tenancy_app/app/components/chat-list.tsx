485d87ade048c024635366287e335b4de5931c11
testA113 <83188384+testA113@users.noreply.github.com>
2024-04-06 12:39:50 +1300

    theme

--- tenancy_app/app/components/chat-list.tsx
+++ tenancy_app/app/components/chat-list.tsx
@@ -1,13 +1,13 @@
 import { Message } from "ai";
 
 import { Source } from "../types";
+import { IconAI, IconUser } from "@/ui/icons";
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
 
 type ParsedMessage = {
-  id: string;
-  role: string;
-  content: string;
   documents?: Source[];
-};
+} & Message;
 
 export function ChatList({ messages }: { messages: Array<Message> }) {
   const parsedMessages = parseMessages(messages);
@@ -15,34 +15,75 @@ export function ChatList({ messages }: { messages: Array<Message> }) {
     <>
       {parsedMessages.map((message) => (
         <div key={message.id} className="whitespace-pre-wrap">
-          {message.role}
-          {message.content}
-          <br />
-          {message.documents &&
-            message.documents.map((document) => (
-              <a
-                key={document.id}
-                href={document.doc_url}
-                className="block text-blue-500 underline"
-                target="_blank"
-              >
-                {`${document.title} (page ${document.page_label})`}
-              </a>
-            ))}
+          {message.role === "user" ? (
+            <UserMessage>{message.content}</UserMessage>
+          ) : (
+            <BotMessage documents={message.documents}>
+              {message.content}
+            </BotMessage>
+          )}
         </div>
       ))}
     </>
   );
 }
 
+export function UserMessage({ children }: { children: ReactNode }) {
+  return (
+    <div className="group relative flex items-start md:-ml-12">
+      <div className="flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border shadow-sm bg-background">
+        <IconUser />
+      </div>
+      <div className="ml-4 flex-1 space-y-2 overflow-hidden px-1">
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export function BotMessage({
+  children,
+  className,
+  documents,
+}: {
+  children: React.ReactNode;
+  className?: string;
+  documents?: Source[];
+}) {
+  return (
+    <div className="flex flex-col">
+      <div
+        className={cn("group relative flex items-start md:-ml-12", className)}
+      >
+        <div className="flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border shadow-sm bg-primary text-primary-foreground">
+          <IconAI />
+        </div>
+        <div className="ml-4 flex-1 space-y-2 overflow-hidden px-1">
+          {children}
+        </div>
+      </div>
+      {documents?.map((document) => (
+        <a
+          key={document.id}
+          href={document.doc_url}
+          className="block text-blue-500 underline"
+          target="_blank"
+        >
+          {`${document.title} (page ${document.page_label})`}
+        </a>
+      ))}
+    </div>
+  );
+}
+
 function parseMessages(messages: Message[]): ParsedMessage[] {
   return messages.map((message) => {
     if (message.role === "user") {
-      return { id: message.id, role: "User: ", content: message.content };
+      return message;
     } else {
       const [documentsString, content] = message.content.split("||||");
       const documents = JSON.parse(documentsString) as Array<Source>;
-      return { id: message.id, role: "Assistant: ", content, documents };
+      return { ...message, content, documents };
     }
   });
 }
--- tenancy_app/app/page.tsx
+++ tenancy_app/app/page.tsx
@@ -1,15 +1,25 @@
 "use client";
 
-import { Message, useChat } from "ai/react";
-import { EmptyScreen } from "./components/empty-screen";
+import { useChat } from "ai/react";
+import { EmptyScreen } from "@/components/empty-screen";
 import { useRef } from "react";
-import { ChatList } from "./components/chat-list";
-import { ChatScrollAnchor } from "./lib/hooks/chat-scroll-anchor";
+import { ChatList } from "@/components/chat-list";
+import { ChatScrollAnchor } from "@/lib/hooks/chat-scroll-anchor";
+import { useEnterSubmit } from "@/lib/hooks/use-enter-submit";
+import { Button } from "./components/ui/button";
+import { IconPlus, IconArrowElbow } from "./components/ui/icons";
+import { Textarea } from "./components/ui/textarea";
+import {
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+} from "./components/ui/tooltip";
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit, setInput } =
     useChat();
-  const inputRef = useRef<HTMLInputElement>(null);
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+  const { formRef, onKeyDown } = useEnterSubmit();
 
   return (
     <div>
@@ -21,16 +31,58 @@ export default function Chat() {
         )}
         <ChatScrollAnchor trackVisibility={true} />
       </div>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input
-            ref={inputRef}
-            className="fixed bottom-0 w-full max-w-2xl p-2 mb-8 border border-gray-300 rounded shadow-xl"
-            value={input}
-            placeholder="Say something..."
-            onChange={handleInputChange}
-          />
-        </form>
+      <div className="fixed inset-x-0 bottom-0 w-full bg-gradient-to-b from-muted/30 from-0% to-muted/30 to-50% duration-300 ease-in-out animate-in dark:from-background/10 dark:from-10% dark:to-background/80 peer-[[data-state=open]]:group-[]:lg:pl-[250px] peer-[[data-state=open]]:group-[]:xl:pl-[300px]">
+        <div className="mx-auto sm:max-w-2xl sm:px-4">
+          <div className="px-4 py-2 space-y-4 border-t shadow-lg bg-background sm:rounded-t-xl sm:border md:py-4">
+            <form onSubmit={handleSubmit} ref={formRef}>
+              <div className="relative flex flex-col w-full px-8 overflow-hidden max-h-60 grow bg-background sm:rounded-md sm:border sm:px-12">
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button
+                      variant="outline"
+                      size="icon"
+                      className="absolute left-0 w-8 h-8 p-0 rounded-full top-4 bg-background sm:left-4"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        window.location.reload();
+                      }}
+                    >
+                      <IconPlus />
+                      <span className="sr-only">New Chat</span>
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>New Chat</TooltipContent>
+                </Tooltip>
+                <Textarea
+                  ref={inputRef}
+                  tabIndex={0}
+                  onKeyDown={onKeyDown}
+                  placeholder="Send a message."
+                  className="min-h-[60px] w-full resize-none bg-transparent px-4 py-[1.3rem] focus-within:outline-none sm:text-sm"
+                  autoFocus
+                  spellCheck={false}
+                  autoComplete="off"
+                  autoCorrect="off"
+                  name="message"
+                  rows={1}
+                  value={input}
+                  onChange={handleInputChange}
+                />
+                <div className="absolute right-0 top-4 sm:right-4">
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Button type="submit" size="icon" disabled={input === ""}>
+                        <IconArrowElbow />
+                        <span className="sr-only">Send message</span>
+                      </Button>
+                    </TooltipTrigger>
+                    <TooltipContent>Send message</TooltipContent>
+                  </Tooltip>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
