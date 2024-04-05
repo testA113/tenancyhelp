28ba12f2830b8179c256563babc4004aab3ef50b
testa113 <testa113>
2024-04-06 11:33:11 +1300

    redo example messages

--- tenancy_app/app/components/empty-screen.tsx
+++ tenancy_app/app/components/empty-screen.tsx
@@ -5,12 +5,25 @@ import { Dispatch, RefObject, SetStateAction } from "react";
 import { Button } from "@/ui/button";
 import { ExternalLink } from "@/ui/external-link";
 import { IconArrowRight } from "@/ui/icons";
+import { cn } from "@/lib/utils";
 
-const exampleMessages: string[] = [
-  "My landlord is withholding my bond, what should I do?",
-  "The toilet is broken and hasn't been fixed for weeks, make a draft email for to my landlord to fix it",
-  "My landlord asked to increase my rent, are they allowed to?",
-  "My landlord entered the property without notice, what are my rights?",
+type ExampleQuery = {
+  message: string;
+  hideWhenSmall?: boolean;
+};
+
+const exampleQueries: Array<ExampleQuery> = [
+  { message: "My landlord is withholding my bond, what should I do?" },
+  { message: "The toilet is broken, make an email to my landlord to fix it." },
+  {
+    message: "My landlord asked to increase my rent, are they allowed to?",
+    hideWhenSmall: true,
+  },
+  {
+    message:
+      "My landlord entered the property without notice, what are my rights?",
+    hideWhenSmall: true,
+  },
 ];
 
 export function EmptyScreen({
@@ -23,14 +36,17 @@ export function EmptyScreen({
   return (
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8 mb-4">
-        <h1 className="mb-2 text-lg font-semibold">Welcome to Tenancy Help!</h1>
+        <h1 className="mb-2 text-lg md:text-2xl font-semibold">
+          Welcome to Tenancy Help!
+        </h1>
         <p className="mb-2 leading-normal text-muted-foreground">
-          This chatbot is a tenancy advisor for tenants in New Zealand.
+          This chatbot is a tenancy advisor specifically designed for tenants in
+          New Zealand.
         </p>
         <p className="mb-2">
-          It's main function is to reslove disputes between tenants and
-          landlords, to avoid escalation. Depending on what you ask, it's advice
-          will reference relevant sources from the{" "}
+          If you're a tenant, its goal is to resolve disputes between you and
+          your landlord/property manager to avoid escalation. Depending on your
+          query, it will provide advice backed by relevant sources from the{" "}
           <ExternalLink href="https://www.legislation.govt.nz/act/public/1986/0120/latest/whole.html?search=sw_096be8ed81d2fc65_23+working+days_25_se&p=1">
             New Zealand Residential Tenancy Act
           </ExternalLink>
@@ -43,25 +59,29 @@ export function EmptyScreen({
         <p className="mb-2 leading-normal text-muted-foreground">
           The chatbot is powered by{" "}
           <ExternalLink href="https://openai.com/chatgpt">ChatGPT</ExternalLink>{" "}
-          which is known to sometimes hallucinate - please double check the
-          advice and references.
+          which may occasionally provide inaccurate information. Please verify
+          all advice and references.
         </p>
-        <p className="leading-normal text-muted-foreground">Try an example:</p>
-        <div className="mt-4 flex flex-col items-start space-y-2 mb-4">
-          {exampleMessages.map((message, index) => (
-            <Button
-              key={index}
-              className="h-auto text-base p-2 w-full text-start justify-start"
-              onClick={() => {
-                setInput(message);
-                inputRef.current?.focus();
-              }}
-            >
-              <IconArrowRight className="mr-2 text-muted-foreground shrink-0" />
-              {message}
-            </Button>
-          ))}
-        </div>
+        <p className="text-gray-500 text-sm">- Made with ♥️ by Ali Harris</p>
+      </div>
+      <h2 className="text-lg font-semibold mb-2">Try an example!</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 mb-4">
+        {exampleQueries.map(({ message, hideWhenSmall }) => (
+          <Button
+            key={message}
+            className={cn(
+              "h-auto text-base p-2 w-full text-start justify-start border",
+              hideWhenSmall && "hidden md:inline-flex"
+            )}
+            onClick={() => {
+              setInput(message);
+              inputRef.current?.focus();
+            }}
+          >
+            <IconArrowRight className="mr-2 text-muted-foreground shrink-0" />
+            {message}
+          </Button>
+        ))}
       </div>
       <p className="leading-normal text-muted-foreground text-[0.8rem] text-center max-w-96 ml-auto mr-auto">
         Note: This chatbot it is not a substitute for professional legal advice.
--- tenancy_app/app/page.tsx
+++ tenancy_app/app/page.tsx
@@ -1,70 +1,37 @@
 "use client";
 
 import { Message, useChat } from "ai/react";
-import { Document } from "./types";
 import { EmptyScreen } from "./components/empty-screen";
 import { useRef } from "react";
+import { ChatList } from "./components/chat-list";
+import { ChatScrollAnchor } from "./lib/hooks/chat-scroll-anchor";
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit, setInput } =
     useChat();
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const parsedMessages = parseMessages(messages);
-
   return (
-    <div className="flex flex-col w-full max-w-2xl py-16 mx-auto stretch">
-      {messages.length > 0 ? (
-        parsedMessages.map((message) => (
-          <div key={message.id} className="whitespace-pre-wrap">
-            {message.role}
-            {message.content}
-            <br />
-            {message.documents &&
-              message.documents.map((document) => (
-                <a
-                  key={document.id}
-                  href={document.doc_url}
-                  className="block text-blue-500 underline"
-                  target="_blank"
-                >
-                  {`${document.title} (page ${document.page_label})`}
-                </a>
-              ))}
-          </div>
-        ))
-      ) : (
-        <EmptyScreen setInput={setInput} inputRef={inputRef} />
-      )}
-
-      <form onSubmit={handleSubmit}>
-        <input
-          ref={inputRef}
-          className="fixed bottom-0 w-full max-w-2xl p-2 mb-8 border border-gray-300 rounded shadow-xl"
-          value={input}
-          placeholder="Say something..."
-          onChange={handleInputChange}
-        />
-      </form>
+    <div>
+      <div className="pb-[200px] pt-4 md:pt-10">
+        {messages.length > 0 ? (
+          <ChatList messages={messages} />
+        ) : (
+          <EmptyScreen setInput={setInput} inputRef={inputRef} />
+        )}
+        <ChatScrollAnchor trackVisibility={true} />
+      </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            ref={inputRef}
+            className="fixed bottom-0 w-full max-w-2xl p-2 mb-8 border border-gray-300 rounded shadow-xl"
+            value={input}
+            placeholder="Say something..."
+            onChange={handleInputChange}
+          />
+        </form>
+      </div>
     </div>
   );
-}
-
-type ParsedMessage = {
-  id: string;
-  role: string;
-  content: string;
-  documents?: Document[];
-};
-
-function parseMessages(messages: Message[]): ParsedMessage[] {
-  return messages.map((message) => {
-    if (message.role === "user") {
-      return { id: message.id, role: "User: ", content: message.content };
-    } else {
-      const [documentsString, content] = message.content.split("||||");
-      const documents = JSON.parse(documentsString) as Array<Document>;
-      return { id: message.id, role: "Assistant: ", content, documents };
-    }
-  });
 }
