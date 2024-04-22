a06984ea0e10be303222807c28a16617868f7594
testA113 <83188384+testA113@users.noreply.github.com>
2024-04-23 00:46:03 +1200

    dockerise next app

--- tenancy_app/app/api/auth/[...nextauth]/route.ts
+++ tenancy_app/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,6 @@
 import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-};
+import { authOptions } from "./auth-config";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
--- tenancy_app/next.config.js
+++ tenancy_app/next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     remotePatterns: [{ hostname: "lh3.googleusercontent.com" }],
   },
+  output: "standalone",
 };
 
 module.exports = nextConfig;
