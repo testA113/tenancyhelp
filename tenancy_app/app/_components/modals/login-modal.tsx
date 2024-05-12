a78676e5a5b6ada45b611ea031821a07c4056856
testA113 <83188384+testA113@users.noreply.github.com>
2024-05-12 15:21:49 +1200

    make login optional

--- tenancy_app/app/_components/modals/login-modal.tsx
+++ tenancy_app/app/_components/modals/login-modal.tsx
@@ -2,6 +2,7 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { signIn } from "next-auth/react";
 import { browserName } from "react-device-detect";
+import { X } from "lucide-react";
 
 import { LoginModalStore } from "@/lib/store/login-modal-store";
 import { Button } from "@/ui/button";
@@ -19,11 +20,20 @@ export const LoginModal = () => {
     <Dialog.Root open={loginModal.isOpen}>
       <Dialog.Portal>
         <Dialog.Overlay className="bg-blackA6 data-[state=open]:animate-overlayShow fixed inset-0 z-20" />
-        <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-2xl translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none overflow-y-auto border border-foreground z-30">
+        <Dialog.Content
+          onPointerDownOutside={loginModal.setClose}
+          className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-2xl translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none overflow-y-auto border border-foreground z-30"
+        >
           <Dialog.Title className="mb-2">
             Welcome to Tenancy Help Insiders!
           </Dialog.Title>
-          <Dialog.Description className="mb-5 leading-normal">
+          <Dialog.Close
+            className="absolute right-5 top-5"
+            onClick={loginModal.setClose}
+          >
+            <X />
+          </Dialog.Close>
+          <div className="mb-5 leading-normal">
             <p className="mb-2">
               You&apos;re the first to get access to Tenancy Help! It&apos;s
               been able to help with my most complicated flatting situations
@@ -45,7 +55,7 @@ export const LoginModal = () => {
                 Privacy Policy
               </InternalLink>
             </div>
-          </Dialog.Description>
+          </div>
           <Button
             variant="default"
             className="p-2 w-full gap-x-2"
